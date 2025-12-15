8f1798fb669aa13bac3afc4687b443620d0ca0bb
rahul <rahul6842479@gmail.com>
2025-12-15 22:46:03 +0530

    KMP algorithm

---
+++ Data Structure/String/KMP.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+
+int main(){
+
+
+    string s = "abcabdabcabcabd";
+
+    vector<int>lps(s.size(),0);
+    int pre = 0, suf = 1;
+
+    while(suf < s.size()){
+        if(s[pre] == s[suf]){
+            lps[suf] = pre + 1;
+            pre++;
+            suf++;
+        }
+
+        else{
+            if(pre == 0){
+                lps[suf] = 0;
+                suf++;
+            }
+            else{
+                pre = lps[pre-1];
+            }
+        }
+    }
+
+    cout<<lps[s.size()-1];
+
+
+
+
+
+    return 0;
+}
