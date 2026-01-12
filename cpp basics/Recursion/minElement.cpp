a5714c56bc0f85ac7d3c0dffe1b69a1734ee311b
rahul <rahulthakur6842479@gmail.com>
2026-01-12 23:48:52 +0530

    added max function as wll

--- cpp basics/Recursion/minElement.cpp
+++ cpp basics/Recursion/minElement.cpp
@@ -11,12 +11,21 @@ int minimum(int arr[], int size){
 
 }
 
+int maximum(int arr[], int size){
+    if(size == 0){
+        return arr[size];
+    }
+
+    return max(arr[size],maximum(arr,size - 1));
+}
+
 int main(){
 
     int arr[] = {4,2,5,1,-1,3,5,3,2,4,5,-3};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    cout<<"minimum element in an array: "<<minimum(arr,size-1);
+    cout<<"minimum element in an array: "<<minimum(arr,size-1)<<endl;
+    cout<<"maximum element in an array: "<<maximum(arr,size-1);
 
 
 
