5b878811d692db7ca64815380ca37a5a2fe9aed7
rahul <rahulthakur6842479@gmail.com>
2026-06-25 22:54:43 +0530

    largest value at each level

---
+++ Data Structure/tree/largestValueEachLevel.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<stack>
+#include<queue>
+#include<climits>
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+Node* tree(){
+
+    int x;
+    cout<<"enter value of node: ";
+    cin>>x;
+
+    if(x == -1){
+        return NULL;
+    }
+    Node* temp = new Node(x);
+    temp->left = tree();
+    temp->right = tree();
+    return temp;
+}
+
+
+
+int main(){
+
+    queue<Node*>q;
+    vector<int>v;
+    int n;
+    int maxi = INT_MIN;
+
+    Node* root = tree();
+    q.push(root);
+    
+    while (!q.empty()){
+        n = q.size();
+        
+        while(n--){
+            Node* temp = q.front();
+            q.pop();
+            maxi = max(maxi,temp->data);
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+
+        v.push_back(maxi);
+        maxi = INT_MIN;
+    }
+
+    for(int i: v){
+        cout<<i<<" ";
+    }
+    
+    
+    
+
+
+    return 0;
+}
